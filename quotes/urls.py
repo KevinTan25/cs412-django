e53ef3101a375d81c23f1416f48cf698c1101ba0
Kevin <ktan03@bu.edu>
2024-09-22 15:37:22 -0400

    Added the urls

--- quotes/urls.py
+++ quotes/urls.py
@@ -6,5 +6,8 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path(r'', views.index, name="index"),
+    path(r'', views.quote, name="quote"), # Main page
+    path('quote', views.quote, name='quote'), # Same as main page
+    path('show_all', views.show_all, name='show_all'), # Show all the quotes/images
+    path('about', views.about, name='about'),  # About page of person
 ]
