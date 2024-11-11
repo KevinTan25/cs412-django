1258adf9bdf5ce031276d43aa7bdb5c192a2d152
Kevin <ktan03@bu.edu>
2024-11-11 00:22:54 -0500

    Added a10_url.txt

--- voter_analytics/views.py
+++ voter_analytics/views.py
@@ -101,7 +101,6 @@ class GraphsView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context = super().get_context_data(**kwargs)
         context['party_affiliations'] = (
             Voter.objects.values_list('party_affiliation', flat=True)
             .distinct()
